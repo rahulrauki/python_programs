06dbd169815390d7943ba88b73ceaa6556b5ecc5
Rahul K <64744770+rahulrauki@users.noreply.github.com>
2023-08-22 01:37:34 +0530

    script -> module

--- extract_yt_links.py
+++ extract_yt_links.py
@@ -1,21 +1,22 @@
-import re, os
-file_path = input("Enter file path: ").strip()
-abs_path = os.path.abspath(path=file_path)
-abs_path = file_path
-# finds https://you && https://www.you
-pattern = re.compile("https:\/\/(?:www\.)?you")
+import re
+def extract_links(file_path, output=None):
+    abs_path = file_path.strip()
+    # finds https://you && https://www.you
+    pattern = re.compile("https:\/\/(?:www\.)?you")
 
-def extract_link(line):
-    word_list = line.strip().split()
-    for word in word_list:
-        return word if word.startswith("https") else None 
+    def extract_link(line):
+        word_list = line.strip().split()
+        for word in word_list:
+            return word if word.startswith("https") else None 
 
-link_list = []
+    link_list = []
 
-with open(abs_path, 'r',encoding="utf-8") as fhand:
-    for line in fhand:
-        if pattern.match(line):
-            link_list.append(extract_link(line))
+    with open(abs_path, 'r',encoding="utf-8") as fhand:
+        for line in fhand:
+            if pattern.match(line):
+                link_list.append(extract_link(line))
 
-with open("links.txt", "w", encoding="utf-8") as fhand:
-    fhand.write("\n".join(link_list))
+    if (output):
+        with open("links.txt", "w", encoding="utf-8") as fhand:
+            fhand.write("\n".join(link_list))
+    else: return link_list
